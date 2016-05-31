2d43ea5cc08c4e80fee31d2bfbb897421e637ede
Qiang <chinkyou@hotmail.com>
2016-05-31 16:27:43 +0800

    JunitTestTimeout

--- myfirstquickstart/src/test/java/com/yourcompany/selenium/myfirstquickstart/TestJunitTimeout.java
+++ myfirstquickstart/src/test/java/com/yourcompany/selenium/myfirstquickstart/TestJunitTimeout.java
@@ -1,18 +1,24 @@
 package com.yourcompany.selenium.myfirstquickstart;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
-public class TestJunitIgnore {
+public class TestJunitTimeout {
 	String message = "Robert";
 	MessageUtil messageUtil = new MessageUtil(message);
 	
-	@Ignore
-	@Test
+	@Test(timeout=500)
 	public void testPrintMessage() {
 		System.out.println("Inside testPrintMessage()");
+		try {
+			TimeUnit.MILLISECONDS.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertEquals(message, messageUtil.printMessage());
 	}
 	
